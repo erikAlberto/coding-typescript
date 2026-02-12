c86c7ac092dbb146a67fc55e0e936c2596147e72
Erik Vargas <erikalberto8492@gmail.com>
2026-02-12 09:28:11 -0400

    adding solve of divisible sum pairs challenge

---
+++ divisible-sum-pairs.ts
@@ -0,0 +1,14 @@
+function divisibleSumPairs(n: number, k: number, ar: number[]): number {
+    // Write your code here
+    let count: number = 0;
+    for (let i = 0; i < n-1; i++){
+        for (let j = i + 1; j < n; j++){
+            if ((ar[i] + ar[j]) % k === 0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(divisibleSumPairs(6, 3, [1,3,2,6,1,2]))
