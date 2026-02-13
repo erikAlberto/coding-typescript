7fd0f054071c024df2cd2837c2510599bec4317e
Erik Vargas <erikalberto8492@gmail.com>
2026-02-13 09:27:49 -0400

    adding solve for the birthday bar challenge

---
+++ the-birthday-bar.ts
@@ -0,0 +1,16 @@
+function birthday(s: number[], d: number, m: number): number {
+    // Write your code here
+    let count = 0;
+    for (let i = 0; i <= s.length - m; i++) {
+        let sum = 0;
+        for (let j = i; j < i + m; j++) {
+            sum += s[j];
+        }
+        if (sum === d) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(birthday([1, 2, 1, 3, 2], 3, 2))
